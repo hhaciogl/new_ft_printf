2a69dce8c7bc12b7af39a72b2f78f943130d69e8
Hasan Hacioglu <hhaciogl@k1m19s06.42istanbul.com.tr>
2024-11-17 05:32:51 +0300

    update

--- ft_printf.c
+++ ft_printf.c
@@ -6,14 +6,18 @@
 /*   By: hhaciogl <hhaciogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 17:40:25 by hhaciogl          #+#    #+#             */
-/*   Updated: 2024/11/16 23:36:09 by hhaciogl         ###   ########.fr       */
+/*   Updated: 2024/11/17 02:55:47 by hhaciogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+#include <stddef.h>
 
 int ft_printf(const char *format, ...)
 {
-	(void)format;
+	va_list a_list;
+	va_start(a_list, format);
+
+	size_t len;
 	return 1;
 }
