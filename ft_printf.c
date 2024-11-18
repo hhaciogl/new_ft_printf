984e2d54f11f69e2bd8446848c6a7f7b78847f3d
Hasan Hacıoğlu <hhaciogl@student.42istanbul.com.tr>
2024-11-18 16:52:18 +0300

    update

--- ft_printf.c
+++ ft_printf.c
@@ -6,18 +6,18 @@
 /*   By: hhaciogl <hhaciogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 17:40:25 by hhaciogl          #+#    #+#             */
-/*   Updated: 2024/11/18 16:41:20 by hhaciogl         ###   ########.fr       */
+/*   Updated: 2024/11/18 16:48:04 by hhaciogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <stddef.h>
 
-int ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
-	va_list a_list;
+	va_list	a_list;
+
 	va_start(a_list, format);
 	(void)format;
-	size_t len;
-	return 1;
+	return (1);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -1,7 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhaciogl <hhaciogl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 16:45:10 by hhaciogl          #+#    #+#             */
+/*   Updated: 2024/11/18 16:46:17 by hhaciogl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 # include "libft/libft.h"
 
-int	ft_printf(const char *, ...);
+int	ft_printf(const char *format, ...);
 
 #endif
