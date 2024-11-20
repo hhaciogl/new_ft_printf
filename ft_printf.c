148928d72fecd30d34f8d7d727205148e9a0078e
hhaciogl <hhaciogl@student.42istanbul.com.tr>
2024-11-21 02:46:17 +0300

    update

--- ft_printf.c
+++ ft_printf.c
@@ -6,21 +6,28 @@
 /*   By: hhaciogl <hhaciogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 17:40:25 by hhaciogl          #+#    #+#             */
-/*   Updated: 2024/11/20 19:49:52 by hhaciogl         ###   ########.fr       */
+/*   Updated: 2024/11/21 02:37:30 by hhaciogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <stddef.h>
 #include "ft_printf.h"
-#include <stdio.h>
+#include <stdlib.h>
+
+union typeholder
+{
+	char *s;
+	int	i;
+};
 
 int	ft_printf(const char *format, ...)
 {
-	va_list	arg_list;
-	size_t	i;
-	size_t	j;
-	int is_F;
+	va_list				arg_list;
+	size_t				i;
+	size_t				j;
+	char				*str;
+	int					is_F;
 
 	i = 0;
 	j = 0;
@@ -28,18 +35,28 @@ int	ft_printf(const char *format, ...)
 	va_start(arg_list, format);
 	while (format[i])
 	{
-		if (format[i] == '%' && format[i+1])
+		if (is_F)
+		{
+			if (format[i] == 's')
+			{
+				str = va_arg(arg_list, char *);
+				ft_putstr_fd(str, 1);
+				j += ft_strlen(str) - 2;
+			}
+			if (format[i] == 'i' || format[i] == 'd')
+			{
+				str = ft_itoa(va_arg(arg_list, int));
+				ft_putstr_fd(str, 1);
+				j += ft_strlen(str) - 2;
+				free(str);
+				str = NULL;
+			}
+
+			is_F = 0;
+		}
+		else if (format[i] == '%' && format[i+1])
 		{
 			is_F = 1;
-			if (format[i+1] == '%' && !is_F)
-			{
-				ft_putchar_fd('%', 1);
-			}
-			else
-			{
-				return (-1);
-			}
-			is_F = 0;
 		}
 		else
 		{
